b8c4406896321be7f8cb4f36d14233d91ba720ca
Pavel <braurs64@gmail.com>
2021-12-04 01:14:37 +0400

    Работа с MUI-GRID

---
+++ src/components/ErrorTable/ErrorTable.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ErrorTable = () => {
+  return (
+    <div className="item ErrorTable">error table</div>
+  );
+};
+
+export default ErrorTable
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-    <div>
-        <h1>Работает!!!!!!!!!</h1>
-    </div>,
+    <App/>,
     document.getElementById('root')
 );
