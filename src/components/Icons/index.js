59b9345cd8452d8494ac1b203af7039e51e9c88b
Pavel <braurs64@gmail.com>
2021-12-07 01:28:06 +0400

    TreeView test

---
+++ src/components/Icons/index.js
@@ -0,0 +1,6 @@
+import { library } from "@material-ui/core";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+library.add (
+  ArrowRightIcon
+);
--- src/components/TreeView/TreeView.jsx
+++ src/components/TreeView/TreeView.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import { Icon } from "@material-ui/core";
+import React, { useState } from "react";
 
-const TreeView = () => {
+const TreeView = ({ data = [] }) => {
   return (
-    <div className="item TreeView">Tree View</div>
+    <div className="d-tree">
+      <ul className="d-flex d-tree-container flex-column">
+        {data.map(tree => (
+          <TreeNode node={tree} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default TreeView
+const TreeNode = ({node}) => {
+  const [childVisible, setChildVisiblity] = useState(false);
+
+  const hasChild = node.children ? true : false;
+
+  return (
+    <li className="d-tree-node border-0">
+      <div className="d-flex" onClick={(e) => setChildVisiblity((v) => !v )}>
+        {hasChild && (
+
+          <div className={`d-inline d-tree-toggler ${ childVisible ? "active" : "" }`}>
+            <Icon icon="ArrowRight"/>
+          </div>
+        )}
+
+        <div className="col d-tree-head">
+          <i className={`mr-1 ${node.icon}`}> </i>
+          {node.label}
+        </div>
+      </div>
+
+
+      {hasChild && childVisible && (
+      <div className="d-tree-content">
+          <ul className="d-flex d-tree-container flex-column">
+            <TreeView data={node.children}/>
+          </ul>
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default TreeView;
