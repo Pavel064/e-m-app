841a880d9c9fab1b696abff57a675ca5686fe7c6
Pavel <braurs64@gmail.com>
2021-12-10 19:39:35 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from "react";
+import {
+    AuthenticationContextProvider,
+    RepositoryContextProvider,
+    FileContextProvider,
+  } 
+  from 'gitea-react-toolkit';
 import ErrorTable from "./components/ErrorTable/ErrorTable";
 import RussianGlossary from "./components/RussianGlossary/RussianGlossary";
 import TreeView from "./components/TreeView/TreeView";
 import './styles/App.css';
 
 function App() {
+    const [repository, setRepository] = React.useState();
+const [filepath, setFilepath] = React.useState();
+const config = {
+  server: "https://bg.door43.org",
+  tokenid:"PlaygroundTesting",
+};
+
     return (
-        <div className="App">
+        <AuthenticationContextProvider>
+  <RepositoryContextProvider
+    repository={repository}
+    onRepository={setRepository}
+    config={config}
+    full_name='bsa/errors'
+    branch='master'
+  >
+    <FileContextProvider
+      filepath={filepath}
+      onFilepath={setFilepath}
+    >
+                 <div className="App">
             <div className="grid-container">
                 <TreeView/>
                 <ErrorTable/>
                 <RussianGlossary/>
             </div>
         </div>
+
+    </FileContextProvider>
+  </RepositoryContextProvider>
+</AuthenticationContextProvider>
+       
     );
 }
 
--- src/components/TreeView/TreeView.jsx
+++ src/components/TreeView/TreeView.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+import {RepositoryContext, FileContext} from 'gitea-react-toolkit';
 
 const TreeView = () => {
+  const { state: repo, component: repoComponent } = useContext(RepositoryContext);
+  const { state: file, component: fileComponent } = useContext(FileContext);
+
   return (
-    <div className="item TreeView">Tree View</div>
+    <div className="item TreeView">
+      {(!repo && repoComponent) || fileComponent}
+      </div>
   );
 };
 
-export default TreeView
+export default TreeView;
