597b9661eace1452f157b5e7915652ab996a6d00
Pavel <braurs64@gmail.com>
2021-12-27 11:15:09 +0400

    fix "delimiter=tab"

--- src/components/ErrorTable/ErrorTable.jsx
+++ src/components/ErrorTable/ErrorTable.jsx
@@ -1,22 +1,19 @@
-import React, { useContext } from "react";
-import {FileContext} from 'gitea-react-toolkit';
+import React, { useContext } from 'react';
+import { FileContext } from 'gitea-react-toolkit';
 import { CsvToHtmlTable } from 'react-csv-to-table';
 
-
 const ErrorTable = () => {
-  
-  const {state: file} = useContext(FileContext);
-
+  const { state: file } = useContext(FileContext);
+  const tab = "\t";
   return (
     <div className="item ErrorTable">
-      {file?.content || 'ErrorTable'}
       <CsvToHtmlTable
-  data={file?.content}
-  csvDelimiter="\t"
-  tableClassName="table table-striped table-hover"
-/>     
+        data={file?.content}
+        csvDelimiter= {tab}
+        tableClassName="table table-striped table-hover"
+      />
     </div>
   );
 };
 
-export default ErrorTable
+export default ErrorTable;
