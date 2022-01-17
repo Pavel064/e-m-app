7b6ca8d5569953a7882f6887b9dcdc9712c3f0ca
Pavel <braurs64@gmail.com>
2022-01-17 19:13:17 +0400

    wip

--- src/components/RussianGlossary/RussianGlossary.jsx
+++ src/components/RussianGlossary/RussianGlossary.jsx
@@ -1,25 +1,40 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { toJSON } from 'usfm-js';
+import { usfmJS, toJSON } from 'usfm-js';
+import { Verse } from 'scripture-resources-rcl';
 
 const baseURL = 'https://git.door43.org/ru_gl/ru_rob/raw/branch/master/08-RUT.usfm';
 
 const RussianGlossary = () => {
   const [bookData, setBookData] = useState({});
+  const [verseObjects, setVersObjects] = useState([]);
+  const [reference, setReference] = useState({});
 
-  function getBookText() {
-    axios
-      .get(baseURL)
-      .then((response) => {
-        const jsonData = toJSON(response.data);
-        setBookData(jsonData);
-      });
-  }
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      const usfmJSON = toJSON(response.data);
+      const { chapters } = usfmJSON;
+      const chapterKey = '1';
+      const verseKey = '3';
+      setVersObjects(chapters[chapterKey][verseKey].verseObjects);
+      setReference({ book: 'tit', chapter: chapterKey, verse: verseKey });
+
+    });
+  }, []) 
+
 
   return (
     <div className="item RussianGlossary">
-      <button onClick={getBookText}>GET BOOKTEXT</button>
-      {bookData || ' No data'}
+
+      <div>
+        <Verse
+          verseKey={3}
+          verseObjects={verseObjects}
+          reference={reference}
+          showUnsupported
+        />
+
+      </div>
     </div>
   );
 };
