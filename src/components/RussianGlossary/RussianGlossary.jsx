575535ab9743fd059e54ee6be665776b4378b825
Pavel <braurs64@gmail.com>
2022-01-18 18:54:57 +0400

    chapter

--- src/components/RussianGlossary/RussianGlossary.jsx
+++ src/components/RussianGlossary/RussianGlossary.jsx
@@ -1,39 +1,37 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { usfmJS, toJSON } from 'usfm-js';
-import { Verse } from 'scripture-resources-rcl';
+import { toJSON } from 'usfm-js';
+import { Verses } from 'scripture-resources-rcl';
 
 const baseURL = 'https://git.door43.org/ru_gl/ru_rob/raw/branch/master/08-RUT.usfm';
 
 const RussianGlossary = () => {
-  const [bookData, setBookData] = useState({});
-  const [verseObjects, setVersObjects] = useState([]);
-  const [reference, setReference] = useState({});
+
+  const [chapter, setChapter] = useState({});
+  const [chapterKey, setChapterKey] = useState('1');
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       const usfmJSON = toJSON(response.data);
       const { chapters } = usfmJSON;
-      const chapterKey = '1';
-      const verseKey = '3';
-      setVersObjects(chapters[chapterKey][verseKey].verseObjects);
-      setReference({ book: 'tit', chapter: chapterKey, verse: verseKey });
-
+      
+      setChapter(chapters[chapterKey]);
+      console.log(chapters);
     });
-  }, []) 
-
+  }, [chapterKey]);
 
   return (
     <div className="item RussianGlossary">
-
       <div>
-        <Verse
-          verseKey={3}
-          verseObjects={verseObjects}
-          reference={reference}
-          showUnsupported
-        />
+        {chapter === {}?'':(
+          <Verses
+          verses={chapter}
+          />
+        )}
+        
+        
 
+       
       </div>
     </div>
   );
