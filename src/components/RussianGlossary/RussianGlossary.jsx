f5b53986a25f326ff34502cda55003c3d960a3dd
valyukhov <alexzed@bk.ru>
2022-01-18 18:33:27 +0300

    WIP

--- src/components/RussianGlossary/RussianGlossary.jsx
+++ src/components/RussianGlossary/RussianGlossary.jsx
@@ -1,37 +1,35 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { toJSON } from 'usfm-js';
-import { Verses } from 'scripture-resources-rcl';
+import { Chapters } from 'scripture-resources-rcl';
 
 const baseURL = 'https://git.door43.org/ru_gl/ru_rob/raw/branch/master/08-RUT.usfm';
 
 const RussianGlossary = () => {
-
-  const [chapter, setChapter] = useState({});
+  const [chapter, setChapter] = useState([]);
   const [chapterKey, setChapterKey] = useState('1');
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       const usfmJSON = toJSON(response.data);
       const { chapters } = usfmJSON;
-      
-      setChapter(chapters[chapterKey]);
-      console.log(chapters);
+
+      setChapter(chapters[1]);
+      // console.log(chapters);
     });
   }, [chapterKey]);
+
+  console.log(chapter[1]);
 
   return (
     <div className="item RussianGlossary">
       <div>
-        {chapter === {}?'':(
-          <Verses
-          verses={chapter}
-          />
+        Chapters
+        {chapter.length > 0 ? (
+          <Chapters chapters={chapter} paragraphs showUnsupported />
+        ) : (
+          ''
         )}
-        
-        
-
-       
       </div>
     </div>
   );
