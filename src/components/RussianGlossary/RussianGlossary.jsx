fc8776c6d3ecd69bfb64f33d911d27a121291320
Pavel <braurs64@gmail.com>
2022-01-19 00:47:14 +0400

    WIP chapterS

--- src/components/RussianGlossary/RussianGlossary.jsx
+++ src/components/RussianGlossary/RussianGlossary.jsx
@@ -1,37 +1,36 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { toJSON } from 'usfm-js';
-import { Verses } from 'scripture-resources-rcl';
+import { Chapters } from 'scripture-resources-rcl';
 
 const baseURL = 'https://git.door43.org/ru_gl/ru_rob/raw/branch/master/08-RUT.usfm';
 
 const RussianGlossary = () => {
-
-  const [chapter, setChapter] = useState({});
-  const [chapterKey, setChapterKey] = useState('1');
+  const [chapter, setChapter] = useState([]);
+  // const [chapterKey, setChapterKey] = useState('1');
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       const usfmJSON = toJSON(response.data);
       const { chapters } = usfmJSON;
-      
-      setChapter(chapters[chapterKey]);
-      console.log(chapters);
+
+      setChapter(chapters);
+      // console.log(chapters);
     });
-  }, [chapterKey]);
+  }, []);
+
+  console.log(chapter[1]);
 
   return (
     <div className="item RussianGlossary">
+      <div> Chapters</div>
       <div>
-        {chapter === {}?'':(
-          <Verses
-          verses={chapter}
-          />
-        )}
-        
-        
-
        
+        {chapter ? (
+          <Chapters chapters={chapter} paragraphs showUnsupported />
+        ) : (
+          'pusto'
+        )}
       </div>
     </div>
   );
