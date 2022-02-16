76428e282ff06d32d6bfdec9ba7dd53a3d55ee05
Pavel <braurs64@gmail.com>
2022-02-16 22:13:01 +0400

    fix

--- src/components/ErrorTable/ErrorTable.jsx
+++ src/components/ErrorTable/ErrorTable.jsx
@@ -5,6 +5,7 @@ import '../../styles/App.css';
 import { useState } from 'react';
 import RussianGlossary from '../../components/RussianGlossary/RussianGlossary';
 import { Paper } from '@material-ui/core';
+import { bookNames } from '../../common/ProjectData';
 
 const ErrorTable = () => {
   const { state: file } = useContext(FileContext);
@@ -23,74 +24,7 @@ const ErrorTable = () => {
       const nameBook = getNameBook[0];
       setChapter(parseInt(value.split(':')[0]));
 
-      const bookNames = {
-        GEN: '01-GEN',
-        EXO: '02-EXO',
-        LEV: '03-LEV',
-        NUM: '04-NUM',
-        DEU: '05-DEU',
-        JOS: '06-JOS',
-        JDG: '07-JDG',
-        RUT: '08-RUT',
-        '1SA': '09-1SA',
-        '2SA': '10-2SA',
-        '1KI': '11-1KI',
-        '2KI': '12-2KI',
-        '1CH': '13-1CH',
-        '2CH': '14-2CH',
-        EZR: '15-EZR',
-        NEH: '16-NEH',
-        EST: '17-EST',
-        JOB: '18-JOB',
-        PSA: '19-PSA',
-        PRO: '20-PRO',
-        ECC: '21-ECC',
-        SNG: '22-SNG',
-        ISA: '23-ISA',
-        JER: '24-JER',
-        LAM: '25-LAM',
-        EZK: '26-EZK',
-        DAN: '27-DAN',
-        HOS: '28-HOS',
-        JOL: '29-JOL',
-        AMO: '30-AMO',
-        OBA: '31-OBA',
-        JON: '32-JON',
-        MIC: '33-MIC',
-        NAM: '34-NAM',
-        HAB: '35-HAB',
-        ZEP: '36-ZEP',
-        HAG: '37-HAG',
-        ZEC: '38-ZEC',
-        MAL: '39-MAL',
-        MAT: '41-MAT',
-        MRK: '42-MRK',
-        LUK: '43-LUK',
-        JHN: '44-JHN',
-        ACT: '45-ACT',
-        ROM: '46-ROM',
-        '1CO': '47-1CO',
-        '2CO': '48-2CO',
-        GAL: '49-GAL',
-        EPH: '50-EPH',
-        PHP: '51-PHP',
-        COL: '52-COL',
-        '1TH': '53-1TH',
-        '2TH': '54-2TH',
-        '1TI': '55-1TI',
-        '2TI': '56-2TI',
-        TIT: '57-TIT',
-        PHM: '58-PHM',
-        HEB: '59-HEB',
-        JAS: '60-JAS',
-        '1PE': '61-1PE',
-        '2PE': '62-2PE',
-        '1JN': '63-1JN',
-        '2JN': '64-2JN',
-        '3JN': '65-3JN',
-        JUD: '66-JUD',
-        REV: '67-REV',
-      };
+      
       const _bookURL = `https://git.door43.org/ru_gl/${getOwner}/raw/branch/master/${bookNames[nameBook]}.usfm`;
       setBookURL(_bookURL);
       setShowRG(true);
